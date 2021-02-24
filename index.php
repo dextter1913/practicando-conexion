876cab09a364e10c9c945d01b42f625794cedd53
dextter1913 <Clau32usa@gmai.com>
2021-02-24 13:55:55 -0500

    Actualizacion

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
     <title>Practicando</title>
 </head>
 <body>
+    <center><h1>Practicando conexion</h1>
+    <form action="index.php" method="post">
+    <label for="nombre">Nombre:</label>
+    <input type="text" name="Nombre" id="nombre"><br><br>
+    <label for="apellido">Apelido:</label>
+    <input type="text" name="Apellido" id="apellido"><br><br>
+    <label for="telefono">Telefono:</label>
+    <input type="text" name="Telefono" id="telefono"><br><br>
+    <input type="submit" value="Registrar">
+    </form>
     
+    </center>
+
 </body>
 </html>
